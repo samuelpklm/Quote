dc78fc78527b545337d568e520a1472d260d5a63
samuelpklm <samuelr76@gmail.com>
2023-02-16 03:34:50 +0100

    hola

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Tarjeta from './features/card/Card';
+
+
+function App() {
+  return (
+    <Tarjeta/>
+  );
+}
+
+export default App;
+
+
+
+
