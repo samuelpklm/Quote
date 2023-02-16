f831f954ef24121716aed9b951a8dc66794f638d
samuelpklm <samuelr76@gmail.com>
2023-02-16 14:06:54 +0100

    Updates

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import cardReducucer from '../features/card/CardSlice';
-
-export const store = configureStore({
-  reducer: {
-    mensaje: cardReducucer,
-  },
-});
